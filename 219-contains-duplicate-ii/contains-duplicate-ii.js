c4a208a5aba2a3fe79c8df23972a40a576e99f75
Abhishek Akkal <akkalabhishek@gmail.com>
2026-06-24 08:48:51 +0530

    Time: 45 ms (26.11%) | Memory: 78 MB (60.58%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+    let map = [];
+
+    for (let i = 0; i < nums.length; i++) {
+
+        if (map[nums[i]] !== undefined) {
+            if (Math.abs(i - map[nums[i]]) <= k) {
+                return true;
+            }
+        }
+
+        map[nums[i]] = i;
+    }
+    return false;
+};
