05295bc5c45439e745744e478a2902013931cb87
Abhishek Akkal <akkalabhishek@gmail.com>
2026-07-23 06:53:01 +0530

    Time: 5 ms (77.83%) | Memory: 57.4 MB (74.65%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    const charMap = new Map();
+    let maxLength = 0;
+    let left = 0;
+    
+    for (let right = 0; right < s.length; right++) {
+        const currentChar = s[right];
+        
+        if (charMap.has(currentChar) && charMap.get(currentChar) >= left) {
+            left = charMap.get(currentChar) + 1;
+        }
+        
+        charMap.set(currentChar, right);
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+    
+    return maxLength;
+};
