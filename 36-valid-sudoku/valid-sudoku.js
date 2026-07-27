337c4ccded9d67dca8b983182e4ea6306fdde637
Abhishek Akkal <akkalabhishek@gmail.com>
2026-07-27 13:01:34 +0530

    Time: 2 ms (95.62%) | Memory: 56.7 MB (63.66%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.js
@@ -0,0 +1,28 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+    const rows = Array.from({ length: 9 }, () => new Array(10).fill(false));
+    const cols = Array.from({ length: 9 }, () => new Array(10).fill(false));
+    const boxes = Array.from({ length: 9 }, () => new Array(10).fill(false));
+    
+    for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 9; c++) {
+            if (board[r][c] === '.') continue;
+            
+            let val = board[r][c] - '0';
+            let boxIndex = Math.floor(r / 3) * 3 + Math.floor(c / 3);
+            
+            if (rows[r][val] || cols[c][val] || boxes[boxIndex][val]) {
+                return false;
+            }
+            
+            rows[r][val] = true;
+            cols[c][val] = true;
+            boxes[boxIndex][val] = true;
+        }
+    }
+    
+    return true;
+};
