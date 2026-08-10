cd1d0ad63f27065e5ff38dc1e5f4d66932a95be2
Abhishek Akkal <akkalabhishek@gmail.com>
2026-08-10 07:19:00 +0530

    Time: 3 ms (60.28%) | Memory: 60.3 MB (34.67%) - LeetSync

---
+++ 39-combination-sum/combination-sum.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    const result = [];
+    
+    const backtrack = (index, currentPath, currentSum) => {
+        if (currentSum === target) {
+            result.push([...currentPath]);
+            return;
+        }
+        
+        if (currentSum > target) {
+            return;
+        }
+        
+        for (let i = index; i < candidates.length; i++) {
+            currentPath.push(candidates[i]);
+            backtrack(i, currentPath, currentSum + candidates[i]);
+            currentPath.pop();
+        }
+    };
+    
+    backtrack(0, [], 0);
+    return result;
+};
