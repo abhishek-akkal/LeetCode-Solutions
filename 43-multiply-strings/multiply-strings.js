f7958101991d89411bc00d3d5b263009e6f38247
Abhishek Akkal <akkalabhishek@gmail.com>
2026-08-13 16:43:19 +0530

    Time: 8 ms (40.11%) | Memory: 55.7 MB (60.44%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.js
@@ -0,0 +1,26 @@
+var multiply = function(num1, num2) {
+    if (num1 === '0' || num2 === '0') return '0';
+    
+    const m = num1.length;
+    const n = num2.length;
+    const pos = new Array(m + n).fill(0);
+    
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+            const mul = (num1[i] - '0') * (num2[j] - '0');
+            const p1 = i + j;
+            const p2 = i + j + 1;
+            
+            const sum = mul + pos[p2];
+            
+            pos[p2] = sum % 10;
+            pos[p1] += Math.floor(sum / 10);
+        }
+    }
+    
+    while (pos[0] === 0) {
+        pos.shift();
+    }
+    
+    return pos.join('');
+};
