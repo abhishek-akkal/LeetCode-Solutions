e75ded270a15bd60c66003b2ca6a321bdd7938f9
Abhishek Akkal <akkalabhishek@gmail.com>
2026-09-03 06:54:29 +0530

    Remove function documentation comments

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -1,8 +1,3 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {void} Do not return anything, modify nums in-place instead.
- */
 var rotate = function(nums, k) {
     const n = nums.length;
     k = k % n; 
