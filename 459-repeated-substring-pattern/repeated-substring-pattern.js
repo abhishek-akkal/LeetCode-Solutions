d29ae01e2192306dfc0c7d9ddcb030469b8f8fae
Abhishek Akkal <akkalabhishek@gmail.com>
2026-09-08 23:07:39 +0530

    Update repeated-substring-pattern.js

--- 459-repeated-substring-pattern/repeated-substring-pattern.js
+++ 459-repeated-substring-pattern/repeated-substring-pattern.js
@@ -1,7 +1,3 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
 var repeatedSubstringPattern = function(s) {
     const doubled = s + s;
     const sliced = doubled.slice(1, doubled.length - 1);
