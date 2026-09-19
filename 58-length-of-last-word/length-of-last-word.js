95f98cf2a8ffc4f655e11d2a20a208808e5c43cd
Abhishek Akkal <akkalabhishek@gmail.com>
2026-09-19 17:41:23 +0530

    Time: 0 ms (100.00%) | Memory: 52.8 MB (86.65%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -5,15 +5,17 @@
 var lengthOfLastWord = function(s) {
     let length = 0;
     let i = s.length - 1;
-    
+
+    // Skip trailing spaces
     while (i >= 0 && s[i] === ' ') {
         i--;
     }
-    
+
+    // Count characters of the last word
     while (i >= 0 && s[i] !== ' ') {
         length++;
         i--;
     }
-    
+
     return length;
 };
